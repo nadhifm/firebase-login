f0f34c1caab854162f9c46acb7192080b384b32f
Nadhih Mahardika <awandi8991@gmail.com>
2021-10-12 08:49:35 +0700

    update recaptcha

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-firebase.auth().onAuthStateChanged(function(user) {
+firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
     // User is signed in.
     if (user.emailVerified) {
       document.getElementById("user_div").style.display = "block";
       document.getElementById("login_div").style.display = "none";
       var email_id = user.email;
-      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+      document.getElementById("user_para").innerHTML =
+        "Welcome User : " + email_id;
     } else {
       window.alert("Verify you email");
       logout();
@@ -14,45 +15,58 @@ firebase.auth().onAuthStateChanged(function(user) {
     // No user is signed in.
     document.getElementById("user_div").style.display = "none";
     document.getElementById("login_div").style.display = "block";
-
   }
 });
 
-function login(){
 
+function login() {
+  
+var recaptcha_response = grecaptcha.getResponse();
+  if (recaptcha_response.length == 0) {
+    window.alert("Recaphta required");
+    return false;
+  }
   var userEmail = document.getElementById("email_field").value;
   var userPass = document.getElementById("password_field").value;
 
-  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-
-    window.alert("Error : " + errorMessage);
-
-  });
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(userEmail, userPass)
+    .catch(function (error) {
+      var errorMessage = error.message;
+      grecaptcha.reset();
+      window.alert("Error : " + errorMessage);
+    });
+  return true;
 }
 
-function register(){
-
+function register() {
+var recaptcha_response = grecaptcha.getResponse();
+  if (recaptcha_response.length == 0) {
+    window.alert("Recaptcha required");
+    return false;
+  }
   var userEmail = document.getElementById("email_field").value;
   var userPass = document.getElementById("password_field").value;
 
-  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass)
-  .then((userCredential) => {
-    // Signed in 
-    var user = userCredential.user;
-    user.sendEmailVerification()
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-
-    window.alert("Error : " + errorMessage);
-
-  });
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(userEmail, userPass)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      user.sendEmailVerification();
+    })
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      grecaptcha.reset();
+      window.alert("Error : " + errorMessage);
+    });
+  return true;
 }
 
-function logout(){
+function logout() {
   firebase.auth().signOut();
 }
